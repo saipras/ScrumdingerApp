5fbf12fef67e265de71d9a339f48d2683c37d083
saiprasad <saiprasadrekha881@gmail.com>
2023-12-01 16:50:51 +0530

    Initial Commit

---
+++ ScrumdingerApp/ScrumdingerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerAppApp.swift
+//  ScrumdingerApp
+//
+//  Created by Sai Prasad Rekha on 01/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
